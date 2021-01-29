cee5083caac8ba0aab46ace6a82118bef2551573
Kirill Panyushin <Kirill_Panyushin@epam.com>
2021-01-29 11:39:29 +0100

    Change auth to API key, remove unnecessary code

--- app/sheets.js
+++ app/sheets.js
@@ -1,64 +1,8 @@
-const fs = require('fs');
-const dotenv = require('dotenv');
-const readline = require('readline');
+require('dotenv').config();
 const { google } = require('googleapis');
 
-dotenv.config();
-
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
-const TOKEN_PATH = 'token.json';
-let sheets;
 const spreadsheetId = process.env.SPREADSHEET_ID;
-
-fs.readFile('../credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  authorize(JSON.parse(content), (auth) => {
-    sheets = google.sheets({ version: 'v4', auth });
-  });
-});
-
-function authorize(credentials, callback) {
-  const { client_secret, client_id, redirect_uris } = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-    client_id,
-    client_secret,
-    redirect_uris[0]
-  );
-
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
-}
-
-function getNewToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err)
-        return console.error(
-          'Error while trying to retrieve access token',
-          err
-        );
-      oAuth2Client.setCredentials(token);
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
-}
+const sheets = google.sheets({ version: 'v4', auth: process.env.GOOGLE_API_KEY });
 
 module.exports.addExpense = function (data, onErr, onSuccess) {
   if (!data || !data.length) return;
